9c29b277a992ee4b15fdc09cebd738384e486bf9
DayronVarsendan <dayronvarsendan@gmail.com>
2025-06-23 19:09:52 +0200

    Commit 2: Conexion con la base de datos
    se ha realizado la conexion con base  de datos, añadiendo una dependecia al archivo .pom, y los respectivos imports, asi como la URL el USER el PASS para poder conectar

--- reservas-salas/src/main/java/com/ProyectoFinal/reservas_salas/App.java
+++ reservas-salas/src/main/java/com/ProyectoFinal/reservas_salas/App.java
@@ -3,5 +3,6 @@ package com.ProyectoFinal.reservas_salas;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    ConexionBD.conectar();
   }
 }
---
+++ reservas-salas/src/main/java/com/ProyectoFinal/reservas_salas/ConexionBD.java
@@ -0,0 +1,21 @@
+package com.ProyectoFinal.reservas_salas;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class ConexionBD {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/reservas_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "1234";
+
+    public static Connection conectar() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("✅ Conexión exitosa a la base de datos.");
+            return conn;
+        } catch (SQLException e) {
+            System.err.println("❌ Error al conectar: " + e.getMessage());
+            return null;
+        }
+    }
+}
